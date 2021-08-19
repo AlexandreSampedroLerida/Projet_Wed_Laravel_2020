c379aff40f06308b0a56e6e552f8dcb4f4429624
Alexandre <alex.sampedrolerida@gmail.com>
2021-08-19 11:49:56 +0200

    Tables Voyager + Models

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+use App\Http\Controllers\PostController;
+Route::resource('posts', PostController::class);
 
 
 Route::group(['prefix' => 'admin'], function () {
